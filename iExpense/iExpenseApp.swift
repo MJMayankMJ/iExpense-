b5e27d31e9c6af82282748f8d8ea9b1e15024193
MJMayankMJ <226301131@gkv.ac.in>
2024-09-07 14:26:20 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mayank Jangid on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
