f283b3d1647ee3ee127526c5a877dddb0db27318
MJMayankMJ <226301131@gkv.ac.in>
2024-09-22 15:21:55 +0530

    Inital Commit

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,96 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct MealExpenseDesign: ViewModifier{
+    let cost: Double
+    func body(content: Content) -> some View {
+        if cost < 10 {
+            content
+                .foregroundStyle(.green)
         }
-        .padding()
+        else if cost >= 10 && cost < 100 {
+            content
+                .foregroundStyle(.cyan)
+        }
+        else{
+            content
+                .foregroundStyle(.red)
+        }
+    }
+}
+
+extension View{
+    func mealExpenseDesign(cost: Double) -> some View{
+        self.modifier(MealExpenseDesign(cost: cost))
+    }
+}
+
+struct ExpenseItem: Identifiable, Codable{
+    //Identifiable is there so to uniquely identify each ExpenseItem (also now we can remove id: \.id in ForEach loop
+    //Codable allows u to archive  and unarchiver through encode and decode.....
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expense {
+    var items = [ExpenseItem]() {
+        //to decode -- we first encode than save it on the phone
+        // to encode -- first we get the data from storage and than decode the data and than show it...
+        didSet{
+            if let encodedItems = try? JSONEncoder().encode(items){
+                UserDefaults.standard.set(encodedItems, forKey: "Items")
+            }
+        }
+    }
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems){
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var expenses = Expense()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List{
+                ForEach(expenses.items) { item in
+                    HStack{
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                    .mealExpenseDesign(cost: item.amount)
+                }
+                .onDelete(perform: removeItem)
+            }
+            .navigationTitle("iExpense")
+            .toolbar{
+                Button("Add Items", systemImage: "plus"){
+                    showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    func removeItem(at offset : IndexSet){
+        expenses.items.remove(atOffsets: offset)
     }
 }
 
